7681ae7a7d483cac62538be67a75f8632620978d
Sarah Phillips <snphillips@gmail.com>
2019-07-29 15:12:17 -0400

    weather info displays

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 // When the submit button is clicked, a fetch call is made to
 // the open weather map API, using the user's inputted zip.
+
 // The clearWeatherData & clearErrorMessage functions are to
 // account for edge cases, when a user inputs a bad zip and
 // and receives error messages, then makes an other attempt.
@@ -13,7 +14,6 @@
 
 
     var zip = document.getElementById("zip").value
-
     console.log("zip button clicked and zip is: " + zip)
 
     function fetchCall(zip){
@@ -31,22 +31,37 @@
     })
   }
 
-  fetchCall(zip)
+   fetchCall(zip)
 
 
-    // Parsing the weather data from the returned JSON
-  function getWeatherData(res){
-    let location = res.name
-    let currentTemp = Math.round(res.main.temp)
-    let tempMin = Math.round(res.main.temp_min)
-    let tempMax = Math.round(res.main.temp_max)
-    let humidity = res.main.humidity
-    let weather = res.weather[0].description
-    // using moment.js to convert from epoch unix time
-    let sunrise = moment.unix(res.sys.sunrise).format('h:mm a')
-    let sunset = moment.unix(res.sys.sunset).format('h:mm a')
-    // updateWeather(location, weather, currentTemp, tempMin, tempMax, humidity, sunrise, sunset)
-    console.log(location, currentTemp, tempMin, tempMax, humidity, weather, sunrise, sunset)
+   // Parsing the weather data from the returned JSON
+   function getWeatherData(res){
+     let location = res.name
+     let currentTemp = Math.round(res.main.temp)
+     let tempMin = Math.round(res.main.temp_min)
+     let tempMax = Math.round(res.main.temp_max)
+     let humidity = res.main.humidity
+     let weather = res.weather[0].description
+     // using moment.js to convert from epoch unix time
+     let sunrise = moment.unix(res.sys.sunrise).format('h:mm a')
+     let sunset = moment.unix(res.sys.sunset).format('h:mm a')
+
+     console.log(location, currentTemp, tempMin, tempMax, humidity, weather, sunrise, sunset)
+     updateWeather(location, weather, currentTemp, tempMin, tempMax, humidity, sunrise, sunset)
+   }
+
+
+   // Display weather conditions
+   function updateWeather(location, weather, currentTemp, tempMin, tempMax, humidity, sunrise, sunset) {
+    document.getElementById('location').innerHTML = `The weather in ${location}:`
+    document.getElementById('weather').innerHTML = `${weather}`
+    document.getElementById('current').innerHTML = `Temperature: ${currentTemp} °F`
+    document.getElementById('min').innerHTML = `Low: ${tempMin} °F`
+    document.getElementById('max').innerHTML = `High: ${tempMax} °F`
+    document.getElementById('humidity').innerHTML = `Humidity: ${humidity}%`
+    document.getElementById('sunrise').innerHTML = `Sunrise: ${sunrise}`
+    document.getElementById('sunset').innerHTML = `Sunset: ${sunset}`
+  // updateWeatherGIF();
   }
 
 

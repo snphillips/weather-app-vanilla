28909e84280b353b5be384bf86f8d42f8ace90f6
Sarah Phillips <snphillips@gmail.com>
2022-05-15 14:12:53 -0400

    create weather gif array of objects

---
+++ js/clearWeatherData.js
@@ -0,0 +1,12 @@
+/* ===============================
+  Clears the weather data, in case there was a previous query.
+  =============================== */
+export function clearWeatherData() {
+  console.log("clearning data");
+  document.getElementById("location").innerHTML = ` `;
+  document.getElementById("weather").innerHTML = ` `;
+  document.getElementById("current-temp").innerHTML = ` `;
+  document.getElementById("min-temp").innerHTML = ` `;
+  document.getElementById("max-temp").innerHTML = ` `;
+  document.getElementById("humidity").innerHTML = ` `;
+}
---
+++ weather-object.js
@@ -0,0 +1,164 @@
+let urlPrefix = "https://media.giphy.com/media/";
+
+weather = [
+  {
+    weather: "heavy intensity rain",
+    gifDescription: "Al Roker in rain",
+    src: urlPrefix + "xUPGcdhiQf2vbfDCyk/giphy.gif",
+  },
+  {
+    weather: "moderate rain",
+    gifDescription: "Al Roker in rain",
+    src: urlPrefix + "xUPGcdhiQf2vbfDCyk/giphy.gif",
+  },
+  {
+    weather: "rain",
+    gifDescription: "Claymation rain cloud",
+    src: urlPrefix + "hk6czgfmwVJS0/giphy.gif",
+  },
+  {
+    weather: "shower rain",
+    gifDescription: "Guy with remote control",
+    src: urlPrefix + "l4FGkdtLGfpUiBdFC/giphy.gif",
+  },
+  {
+    weather: "thunderstorm",
+    gifDescription: "Thunderstorm in coffee cup",
+    src: urlPrefix + "3oEjIa5lbVSfv8a9s4/giphy.gif",
+  },
+  {
+    weather: "light rain",
+    gifDescription: "Al Roker in light rain",
+    src: urlPrefix + "xUPGcILSlV4VjCZ9PG/giphy.gif",
+  },
+  {
+    weather: "drizzle",
+    gifDescription: "Al Roker in light rain",
+    src: urlPrefix + "xUPGcILSlV4VjCZ9PG/giphy.gif",
+  },
+  {
+    weather: "light intensity drizzle",
+    gifDescription: "Al Roker in light rain",
+    src: urlPrefix + "xUPGcILSlV4VjCZ9PG/giphy.gif",
+  },
+  {
+    weather: "heavy intensity drizzle",
+    gifDescription: "woman in bathing suit, on ground with rain",
+    src: urlPrefix + "",
+  },
+  {
+    weather: "clear sky",
+    gifDescription: "cat flying through sky",
+    src: urlPrefix + "VxbvpfaTTo3le/giphy.gif",
+  },
+  {
+    weather: "sky is clear",
+    gifDescription: "cat flying through sky",
+    src: urlPrefix + "VxbvpfaTTo3le/giphy.gif",
+  },
+  {
+    weather: currentTemp > 90,
+    gifDescription: "Kramer turkey",
+    src: urlPrefix + "5xqKWd6761tkY/giphy.gif",
+  },
+  {
+    weather: currentTemp < 25,
+    gifDescription: "slipping penguin",
+    src: urlPrefix + "BvBEozfsXWWHe/giphy.gif",
+  },
+  {
+    weather: currentTemp > 85 && humidity > 80,
+    gifDescription: "melting popcicle",
+    src: urlPrefix + "xUA7b0fCaEIjdwJXTa/giphy.gif",
+  },
+  {
+    weather: "snow",
+    gifDescription: "Al Roker in snow",
+    src: urlPrefix + "xUPGcChZRE8p2djeiQ/giphy.gif",
+  },
+  {
+    weather: "few clouds",
+    gifDescription: "hanging rocking clouds",
+    src: urlPrefix + "h0VzgrFX9AKXK/giphy.gif",
+  },
+  {
+    weather: "clouds",
+    gifDescription: "",
+    src: urlPrefix + "rrFcUcN3MFmta/giphy.gif",
+  },
+  {
+    weather: "scattered clouds",
+    gifDescription: "sloth wearing sunglasses reflecting clouds",
+    src: urlPrefix + "rrFcUcN3MFmta/giphy.gif",
+  },
+  {
+    weather: "broken clouds",
+    gifDescription: "cloud falls on lady",
+    src: urlPrefix + "rrFcUcN3MFmta/giphy.gif",
+  },
+  {
+    weather: "overcast clouds",
+    gifDescription: "black and white moody cloudy sky",
+    src: urlPrefix + "lKKXOCVviOAXS/giphy.gif",
+  },
+  {
+    weather: "dust",
+    gifDescription: "dust gif",
+    src: urlPrefix + "l4pSWPz4u8zQcXjY4/giphy.gif",
+  },
+  {
+    weather: "light snow",
+    gifDescription: "oscar the grouch",
+    src: urlPrefix + "4jGoaiwq9oRri/giphy.gif",
+  },
+  {
+    weather: "mist",
+    gifDescription: "",
+    src: urlPrefix + "apB1oxVnxPaLu/giphy.gif",
+  },
+  {
+    weather: "fog",
+    gifDescription: "glass cup on it's side, fog pouring out",
+    src: urlPrefix + "apB1oxVnxPaLu/giphy.gif",
+  },
+  {
+    weather: "haze",
+    gifDescription: "cat smoke",
+    src: urlPrefix + "HknSLLEbzZCoM/giphy.gif",
+  },
+  {
+    weather: "smoke",
+    gifDescription: "cousin itt smoking",
+    src: urlPrefix + "5xtDarLUQd5DqgXHJks/giphy.gif",
+  },
+  {
+    weather: "sleet",
+    gifDescription: "wet cat",
+    src: urlPrefix + "26BGD4XaoPO3zTz9K/giphy.gif",
+  },
+  {
+    weather: "shower sleet",
+    gifDescription: "wet cat",
+    src: urlPrefix + "26BGD4XaoPO3zTz9K/giphy.gif",
+  },
+  {
+    weather: "",
+    gifDescription: "",
+    src: urlPrefix + "",
+  },
+  {
+    weather: "",
+    gifDescription: "",
+    src: urlPrefix + "",
+  },
+  {
+    weather: "",
+    gifDescription: "",
+    src: urlPrefix + "",
+  },
+  {
+    weather: "",
+    gifDescription: "",
+    src: urlPrefix + "",
+  },
+];
